006fbcc9f0088ce341a300d55f2ac9444cac1547
johncartergonzalez <unmrf0ngh@mozmail.com>
2023-10-03 00:49:07 -0700

    feat: handle mutiple incoming streams stream_handler asynchronously handle TCP streams stream_handler -> mut TcpStream -> 'Result splits the read and write parts and wraps the read into a buffer, loops through the buffer if a line starts with PING, it writes back PONG after handling it clears the buffer

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt}; // more better tokio!!!
+use tokio::{
+    io::{
+        AsyncBufReadExt,
+        AsyncWriteExt,
+        BufReader
+    },
+    net::{
+        TcpStream,
+        TcpListener
+    }}; //tokio magic
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:6379").await?;
+async fn stream_handler(mut stream: TcpStream) -> anyhow::Result<()> {
+    let (read, mut write) = stream.split();
 
-    while let Ok((mut stream, _)) = listener.accept().await {
-        let mut buf = bytes::BytesMut::new();
-        if stream.read(&mut buf).await.is_ok() {
-            println!("{}", String::from_utf8_lossy(&buf));
-            stream.write(b"+PONG\r\n").await.ok();
+    let mut buf_read = BufReader::new(read);
+    let mut buf = String::new();
+
+    while buf_read.read_line(&mut buf).await? > 0 {
+        println!("{}", buf);
+
+        if buf.to_ascii_uppercase().starts_with("PING") {
+            write.write_all(b"+PONG\r\n").await?;
         }
+         buf.clear();
     }
+
     Ok(())
+}
+
+// tokio's runtime for async handling
+#[tokio::main]
+async fn main() -> anyhow::Result<()> { // update fn signature to return anyhow
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    // handle incoming connections
+    loop {
+        let (stream, _addr) = listener.accept().await?;
+        stream_handler(stream).await?;
+    }
 }
